15cd617c94c233838aa6f13b29186e857c422ab6
hilina <hilinaashenafi38@gmail.com>
2025-09-21 11:59:27 +0300

    Add prime checker program

---
+++ prime checker.py
@@ -0,0 +1,15 @@
+num = 29
+is_prime = True
+
+if num < 2:
+    is_prime = False
+else:
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print(num, "is prime")
+else:
+    print(num, "is not prime")
